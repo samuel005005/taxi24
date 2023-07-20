f94417c5425be8b435d55abe15faf72feac5a0f5
Samuel Paez Perez <spaez@viva.com.do>
2023-07-20 09:34:24 -0400

    fix name create trip use case

--- src/trips/application/usecases/index.ts
+++ src/trips/application/usecases/index.ts
@@ -1,6 +1,6 @@
 import GetsAvailableTripsUseCase from './getsAvailableTrips.usecase';
 import CompleteTripUseCase from './completeTrip.usecase';
-import CreateTripUseCase from './createTrips.usecase';
+import CreateTripUseCase from './createTrips.usecase'; 
 import GetTripByIdUseCase from './getTripById.usecase';
 export {
   GetsAvailableTripsUseCase,
